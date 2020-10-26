6d7231ccbe75a60a750fee89fa76b26cab994e77
eliezerladeira <eliezerladeira@yahoo.com.br>
2020-10-25 22:58:28 -0300

    Cadastro de Empresa
    Criação do formulário de cadastro de empresa

--- Projeto/Controle_Estoque.dpr
+++ Projeto/Controle_Estoque.dpr
@@ -5,7 +5,8 @@ uses
   U_Principal in '..\Forms\U_Principal.pas' {frm_Principal},
   U_DM in '..\Forms\U_DM.pas' {DM: TDataModule},
   U_Padrao in '..\Forms\U_Padrao.pas' {frm_Padrao},
-  U_Usuario in '..\Forms\U_Usuario.pas' {frm_Usuario};
+  U_Usuario in '..\Forms\U_Usuario.pas' {frm_Usuario},
+  U_Empresa in '..\Forms\U_Empresa.pas' {frm_Empresa};
 
 {$R *.res}
 
@@ -16,5 +17,6 @@ begin
   Application.CreateForm(TDM, DM);
   Application.CreateForm(Tfrm_Padrao, frm_Padrao);
   Application.CreateForm(Tfrm_Usuario, frm_Usuario);
+  Application.CreateForm(Tfrm_Empresa, frm_Empresa);
   Application.Run;
 end.
